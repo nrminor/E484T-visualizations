0c9c2c09b5759afa4784e463214a26a69f0376a2
Nick Minor <nrminor@wisc.edu>
2022-01-28 14:44:48 -0600

    Learning more about mutations other than E484T

--- Figure2A_consensus_mutations_through_time.R
+++ Figure2A_consensus_mutations_through_time.R
@@ -120,6 +120,22 @@ for (i in 1:length(crude_vcf$PROTEIN_EFFECT)){
   
 }
 
+for (i in 1:nrow(crude_vcf)){
+  
+  if (crude_vcf$PROTEIN_EFFECT[i]=="nonsynonymous"){
+    gene <- crude_vcf$GENE[i]
+    codon <- crude_vcf$CODON_NUMBER[i]
+    aa_sub <- crude_vcf$AA_CHANGE[i]
+    aa_sub <- str_replace(aa_sub, "->", codon)
+    
+    crude_vcf$FULL_SUB_NAME[i] <- paste(gene, aa_sub, sep = " ")
+    
+  } else {
+    crude_vcf$FULL_SUB_NAME[i] <- NA
+  }
+  
+}
+
 # parsing out and sorting the dates
 for (i in 1:nrow(crude_vcf)){
   varseq <- crude_vcf[i,"DATES_DETECTED"]
@@ -176,13 +192,124 @@ for (i in 1:nrow(crude_vcf)){
 }
 crude_vcf$EARLIEST_DAY <- as.numeric(crude_vcf$EARLIEST_DAY)
 
+
+
 crude_vcf <- crude_vcf[,c(1,2,4,5,11:ncol(crude_vcf))]
-crude_vcf <- crude_vcf[,c(1:4, 6:11, 5, 13, 12, 14)]
+crude_vcf <- crude_vcf[,c(1:4, 6:12, 5, 14, 13, 15)]
 colnames(crude_vcf)[1] <- "GENBANK_REFERENCE"
 syn <- crude_vcf[crude_vcf$PROTEIN_EFFECT=="synonymous",] ; syn_count <- nrow(syn)
 nonsyn <- crude_vcf[crude_vcf$PROTEIN_EFFECT=="nonsynonymous",] ; nonsyn_count <- nrow(nonsyn)
 
 paste("There are", nonsyn_count, "nonsynonomous mutations that arose during this infection.", sep = " ")
+
+aa_subs <- crude_vcf[,c(11,10,7,9,5,6,3,2,4,8,14,12,1)]
+aa_subs$DAYS_DETECTED <- str_replace_all(aa_subs$DAYS_DETECTED, ",", ";")
+write.csv(aa_subs, "data/consensus_mutations_codon_aminoacid.csv", 
+          row.names = F)
+
+pdf("visuals/mutation_spacing_by_gene.pdf")
+par(mfrow=c(2,3))
+for (i in unique(nonsyn$GENE)[is.na(unique(nonsyn$GENE))==F]){
+  gene_sub <- nonsyn[nonsyn$GENE==i, ]
+  mut_spacing <- c(0)
+  
+  for (j in 2:nrow(gene_sub)){
+    dist <- gene_sub$POS[j] - gene_sub$POS[j-1]
+    mut_spacing <- c(mut_spacing, dist)
+  }
+  
+  hist(mut_spacing, main = i)
+}
+dev.off()
+
+mutation_density_by_gene <- data.frame("GENE" = unique(crude_vcf$GENE)[is.na(unique(crude_vcf$GENE))==F], 
+                                       "GENE_LENGTH" = NA, 
+                                       "MUTATION_COUNT" = NA,
+                                       "MUTATION_DENSITY" = NA,
+                                       "NONSYN_COUNT" = NA,
+                                       "NONSYN_DENSITY" = NA)
+mutation_density_by_gene$GENE_LENGTH <- c((21555-266), (25384-21563), 
+                                          (26220-25393), (26472-26245),
+                                          (27759-27394), (28259-27894),
+                                          (29533-28274))
+for (i in mutation_density_by_gene$GENE){
+  mutation_density_by_gene[mutation_density_by_gene$GENE==i, "MUTATION_COUNT"] <- nrow(aa_subs[aa_subs$GENE==i,])
+  mutation_density_by_gene[mutation_density_by_gene$GENE==i, "NONSYN_COUNT"] <- nrow(nonsyn[nonsyn$GENE==i,])
+}
+for (i in 1:nrow(mutation_density_by_gene)){
+  mutation_density_by_gene$MUTATION_DENSITY[i] <- mutation_density_by_gene$MUTATION_COUNT[i]/mutation_density_by_gene$GENE_LENGTH[i]
+  mutation_density_by_gene$NONSYN_DENSITY[i] <- mutation_density_by_gene$NONSYN_COUNT[i]/mutation_density_by_gene$GENE_LENGTH[i]
+}
+write.csv(mutation_density_by_gene, "data/mutation_density_per_gene.csv", row.names=F)
+
+density_time <- data.frame("GENE" = rep(unique(crude_vcf$GENE)[is.na(unique(crude_vcf$GENE))==F], times = 8), 
+                           "DAY" = 0,
+                           "GENE_LENGTH" = 0, 
+                           "MUTATION_COUNT" = 0,
+                           "MUTATION_DENSITY" = 0,
+                           "NONSYN_COUNT" = 0,
+                           "NONSYN_DENSITY" = 0)
+
+density_time$GENE_LENGTH <- c((21555-266), (25384-21563), 
+                                          (26220-25393), (26472-26245),
+                                          (27759-27394), (28259-27894),
+                                          (29533-28274))
+density_time <- density_time[order(density_time$GENE),]
+density_time$DAY <- fasta_df$day_of_infection
+
+for (i in unique(density_time$GENE)){
+  
+  vcf <- crude_vcf[is.na(crude_vcf$GENE)==F, ]
+  
+  sub <- density_time[density_time$GENE==i,] ; rownames(sub) <- NULL
+  vcf_sub <- vcf[vcf$GENE==i,] ; rownames(vcf_sub) <- NULL
+  nonsyn_sub <- nonsyn[nonsyn$GENE==i,] ; rownames(nonsyn) <- NULL
+  
+  for (j in sub$DAY){
+    
+    for (k in 1:nrow(vcf_sub)){
+      if (grepl(j, vcf_sub$DAYS_DETECTED[k])){
+        density_time[density_time$GENE==i & density_time$DAY==j,"MUTATION_COUNT"] <- density_time[density_time$GENE==i & density_time$DAY==j,"MUTATION_COUNT"] + 1
+      } else {
+        next
+      }
+    }
+    
+    if (nrow(nonsyn_sub)>1){
+      for (l in 1:nrow(nonsyn_sub)){
+        if (grepl(j, nonsyn_sub$DAYS_DETECTED[l])){
+          density_time[density_time$GENE==i & density_time$DAY==j,"NONSYN_COUNT"] <- density_time[density_time$GENE==i & density_time$DAY==j,"NONSYN_COUNT"] + 1
+        } else {
+          next
+        }
+      }
+    } else {
+      next
+    }
+  }
+}
+for (i in 1:nrow(density_time)){
+  density_time$MUTATION_DENSITY[i] <- density_time$MUTATION_COUNT[i]/density_time$GENE_LENGTH[i]
+  density_time$NONSYN_DENSITY[i] <- density_time$NONSYN_COUNT[i]/density_time$GENE_LENGTH[i]
+}
+write.csv(density_time, "data/mutation_density_per_gene_through_time.csv", row.names=F)
+
+plotting_colors <- c("red", "blue", "green", "yellow", "orange", "purple", "brown")
+pdf("visuals/mutations_density_through_time.pdf")
+par(bty="n")
+plot(1,1, xlim = c(100,400), ylim = c(0,max(density_time$MUTATION_DENSITY)),
+     xlab = "Post-Diagnosis Day", ylab = "Mutation Density (Count/base pairs)",
+     cex.lab=0.9)
+for (i in 1:length(unique(density_time$GENE))){
+  plotting_gene <- density_time[density_time$GENE==unique(density_time$GENE)[i],]
+  lines(plotting_gene$DAY, plotting_gene$MUTATION_DENSITY, col = plotting_colors[i],
+        lwd=3)
+}
+legend(160, 0.006, legend = unique(density_time$GENE), col = plotting_colors, 
+       lty = rep(1, length(unique(density_time$GENE))), 
+       lwd = rep(2, length(unique(density_time$GENE))), 
+       cex = 0.6, bty = 'n',bg="transparent", xpd = T, ncol = 4)
+dev.off()
 
 
 
