a14e44c9b5742fc09220b3b32397ec89f491bac0
Nick Minor <nrminor@wisc.edu>
2022-02-09 15:35:14 -0600

    fixing missing palette and point-adding bugs

--- scripts/Figure2C_neutralization_assay.R
+++ scripts/Figure2C_neutralization_assay.R
@@ -56,6 +56,7 @@ antibody <- read.csv("data/antibody_potency.csv")
 ### -------- #
 pdf(file = "visuals/neutralization_assay.pdf", 
     width = 8, height = 7)
+palette = c("#FFD300", "#0C328A")
 par(bty="n") # deleting the box
 gap.plot(c(1:4), antibody$Chronic, 
          gap=c(1.5,9.5), 
@@ -63,18 +64,21 @@ gap.plot(c(1:4), antibody$Chronic,
          xtics = c(1,2,3,4), xticlab = row.names(antibody),
          xlab = NA, ylab = "IC99 values µg/ml",
          cex.axis = 0.8,
-         col = palette$points[2], pch = 2, lwd = 2, cex = 1.2)
+         col = rep(palette[1], 4), pch = 2, lwd = 2, cex = 1.2)
 grid()
-# lines(c(1:4), antibody$S.614G, col = palette$lines[1], lwd = 2)
-points(c(1:4), antibody$S.614G, col = palette$points[1], pch = 0, cex = 1.2)
-
-legend(3, 2,
+gap.plot(c(1:4), antibody$S.614G, 
+         gap=c(1.5,9.5), 
+         ylim = c(0,10), ytics = c(0.0, 0.5, 1.0, 10),
+         xtics = c(1,2,3,4), xticlab = row.names(antibody),
+         add = TRUE,
+         cex.axis = 0.8,
+         col = rep(palette[2], 4), pch = 0, lwd = 2, cex = 1.2)
+legend(3, 2.1,
        legend = c("S-614G Control Virus", "Chronic Infection Virus"),
        pch = c(0, 2),
-       col = c(palette$points[1], palette$points[2]),
+       col = c(palette[2], palette[1]),
        bty = "n", cex = 0.8, bg="transparent", xpd = T)
-
-# abline(h=seq(1.45,1.575,.001), col="white")
+abline(h=seq(1.45,1.575,.001), col="white")
 axis.break(2,1.5,style="slash")
 dev.off()
 
